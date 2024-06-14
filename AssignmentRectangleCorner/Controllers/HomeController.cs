832befbc0bcddf886cd9e29ead28cd391d1073b4
Shantanu Inagle <shantanuingale2002@gmail.com>
2024-06-14 22:28:58 +0530

    first commit

---
+++ AssignmentRectangleCorner/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+using AssignmentRectangleCorner.Model;
+using AssignmentRectangleCorner.Repository.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AssignmentRectangleCorner.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ITestCaseRepository testcaseRepo;
+        public HomeController(ITestCaseRepository repo)
+        {
+            this.testcaseRepo = repo;
+        }
+
+
+        public IActionResult QuestionPage()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult QuestionPage(TestCaseModel model)
+        {
+            float result = testcaseRepo.PerformTheOperation(model);
+            model.Answer = result;
+            return View(model);
+        }
+
+
+        public IActionResult GetAllRecords()
+        {
+            IEnumerable<TestCaseModel> data = testcaseRepo.GetAllTestCases();
+            return View(data);
+        }
+
+        public IActionResult ClearAllRecords()
+        {
+            testcaseRepo.ClearHistory();
+            return RedirectToAction("GetAllRecords");
+        }
+
+
+    }
+}
